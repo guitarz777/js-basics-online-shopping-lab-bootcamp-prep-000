aa6f2cf38d2055fafe1ce23e55a7415545572d6a
guitarz777@gmail.com <guitarz777@gmail.com>
2018-09-04 21:27:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -65,6 +65,7 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   }else{
     var total = total();
-    cart.
+    cart = []
+    return `Your total cost is $${total}, which will be charged to the card ${cardNumber}`
   }
 }
