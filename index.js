2b3578a75b569fc4e40f3b4e781105de6881df87
guitarz777@gmail.com <guitarz777@gmail.com>
2018-09-04 21:21:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,5 +61,9 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber === isNaN){
+    return "Sorry, we don't have a credit card on file for you."
+  }else{
+    
+  }
 }
