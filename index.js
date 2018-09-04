7260ec4ee113c73d697ba0f32376083334c68b85
guitarz777@gmail.com <guitarz777@gmail.com>
2018-09-04 21:06:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ function total() {
 }
 
 function removeFromCart(item) {
-  for (i=0; i<cart.length; i++){
+  for (let i=0; i<cart.length; i++){
     if (cart[i].itemName===item){
       delete cart[i].itemName
       return cart
