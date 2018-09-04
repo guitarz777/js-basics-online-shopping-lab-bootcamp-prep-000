79faa43ae054f958a79d636310fc7b4d5cc12730
guitarz777@gmail.com <guitarz777@gmail.com>
2018-09-04 20:25:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function viewCart() {
   }else{
     var i = 0;
     while (i<cart.length){
-      str += `${cart[i].itmeName} at $${cart[i].itemPrice}`;
+      str += `${cart[i].itemName} at $${cart[i].itemPrice}`;
       i+=1
       if (i===length.cart){
        str+=`${cart[i].itemName} at $${cart[i].itemPrice}`
