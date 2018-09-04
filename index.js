692d7a9612f3d2d196d1a3c49725d10e25d3da33
guitarz777@gmail.com <guitarz777@gmail.com>
2018-09-04 21:45:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber.isIntiger){
+  if (!cardNumber.isInteger){
     return "Sorry, we don't have a credit card on file for you."
   }else{
     var sum = total();
