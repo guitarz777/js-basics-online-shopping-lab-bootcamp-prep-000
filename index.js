76cedd932d09b924664e60b0052a1e44b5f71bdd
guitarz777@gmail.com <guitarz777@gmail.com>
2018-09-04 17:58:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,12 +10,31 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  itemPrice = []
-  itemPrice[item] = Math.floor.Math.random()*100;
+  var itemName=item
+  var itemPrice=Math.floor(Math.random()*100)
+  var items={item, itemPrice}
+  cart.push(items);
+  return `${item} has been added to your cart.`
+  
 }
 
 function viewCart() {
-  // write your code here
+  var str='In your cart, you have '
+  if (cart.length>0){
+    for (let i=0; i<cart.length; i++){
+      var item = Object.keys(cart[i])
+      var price = Object.values(cart[i])
+      str += `${itemName} at $${pricePrice}`
+      if (i+1<cart.length){
+        str += ", "
+      }else{
+        str += "."
+      }
+    }
+    return str
+  }else{
+    return "Your shopping cart is empty."
+  }
 }
 
 function total() {
