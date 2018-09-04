a29e44bfddb42977e265333a3f598c0ee4dfb565
guitarz777@gmail.com <guitarz777@gmail.com>
2018-09-04 21:50:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   if (Number.isInteger(cardNumber)){
     var sum = total();
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+    return `Your total cost is $${sum}, which will be charged to the card ${cardNumber}.`
     
   }else{
     return "Sorry, we don't have a credit card on file for you."
