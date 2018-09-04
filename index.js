d47394abf4cf72e7853288eb17219ee2e7158f83
guitarz777@gmail.com <guitarz777@gmail.com>
2018-09-04 22:03:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,23 +9,24 @@ function setCart(c) {
   return cart;
 }
 
+
 function addToCart(item) {
-  var itemName=item
-  var itemPrice=Math.floor(Math.random()*100)
-  var items={'itemName': itemName, 'itemPrice' :itemPrice}
+  var itemName=item;
+  var itemPrice=Math.floor(Math.random()*100);
+  var items={'itemName': itemName, 'itemPrice' :itemPrice};
   cart.push(items);
-  return `${item} has been added to your cart.`
-  
+  return `${item} has been added to your cart.`;
 }
+
 
 function viewCart() {
   var str='In your cart, you have '
   if (cart.length<1){
     return "Your shopping cart is empty."
   }else if (cart.length===1){
-    return `${str}${cart[0].itemName} at $${cart[0].itemPrice}.`
+    return `${str}${cart[0].itemName} at $${cart[0].itemPrice}.`;
   }else if (cart.length===2){
-    return `${str}${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
+    return `${str}${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`;
   }else{
     var i = 0;
     while (i<cart.length){
@@ -45,10 +46,10 @@ function viewCart() {
 function total() {
   var total = 0;
   for (let i = 0; i < cart.length; i++){
-    total += cart[i].itemPrice
+    total += cart[i].itemPrice;
   }
   
-  return total
+  return total;
 }
 
 function removeFromCart(item) {
